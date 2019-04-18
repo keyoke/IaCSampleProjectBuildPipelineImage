8d95f1675d1fae69986f2ea2728c98fccb813185
Gareth Emslie <gareth@soft-cor.es>
2019-04-18 16:38:53 +0200

    updates

--- Windows/ServerCore/1803/install.ps1
+++ Windows/ServerCore/1803/install.ps1
@@ -8,7 +8,7 @@ choco install kubernetes-helm --confirm
 # Install kubectl
 # Install-Script -Name install-kubectl -Scope CurrentUser -Force
 c:\windows\temp\install-kubectl.ps1 -Downloadlocation "$($env:ProgramFiles)\kubectl\"
-$env:Path += ";$($env:ProgramFiles)\kubectl\" 
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\kubectl\" , [EnvironmentVariableTarget]::Machine)
 
 # Install aks-engine
 $version = "v0.34.1"
@@ -16,4 +16,4 @@ $archivename = "aks-engine-$version-windows-amd64"
 Invoke-WebRequest -Uri "https://github.com/Azure/aks-engine/releases/download/$version/$archivename.zip" -OutFile "c:\windows\temp\$archivename.zip"
 Expand-Archive -LiteralPath "c:\windows\temp\$archivename.zip" -DestinationPath "c:\windows\temp\" -Force
 Move-Item -Path "c:\windows\temp\$archivename" -Destination "$($env:ProgramFiles)\aks-engine" -Force
-$env:Path += ";$($env:ProgramFiles)\aks-engine\"
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\aks-engine\", [EnvironmentVariableTarget]::Machine)
