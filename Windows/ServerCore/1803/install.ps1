052a7a32f91d86f70485851e74081b358e2b466a
Gareth Emslie <gareth@soft-cor.es>
2019-04-18 14:41:09 +0200

    updates

--- Windows/ServerCore/1803/install.ps1
+++ Windows/ServerCore/1803/install.ps1
@@ -6,11 +6,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install kubernetes-helm --confirm
 
 # Install kubectl
-Install-Script -Name install-kubectl -Scope CurrentUser -Force
-./install-kubectl.ps1 -DownloadLocation "$($env:ProgramFiles)\kubectl\"
+# Install-Script -Name install-kubectl -Scope CurrentUser -Force
+c:\windows\temp\install-kubectl.ps1 -Downloadlocation "$($env:ProgramFiles)\kubectl\"
 $env:Path += ";$($env:ProgramFiles)\kubectl\" 
 
 # Install aks-engine
-Invoke-WebRequest -Uri "https://github.com/Azure/aks-engine/releases/download/v0.34.1/aks-engine-v0.34.1-windows-amd64.zip" -OutFile "c:\temp\aks-engine-v0.34.1-windows-amd64.zip"
-Expand-Archive -LiteralPath "c:\temp\aks-engine-v0.34.1-windows-amd64.zip" -DestinationPath "$($env:ProgramFiles)\aks-engine\"
+Invoke-WebRequest -Uri "https://github.com/Azure/aks-engine/releases/download/v0.34.1/aks-engine-v0.34.1-windows-amd64.zip" -OutFile "c:\windows\temp\aks-engine-v0.34.1-windows-amd64.zip"
+Expand-Archive -LiteralPath "c:\windows\temp\aks-engine-v0.34.1-windows-amd64.zip" -DestinationPath "$($env:ProgramFiles)\aks-engine\" -Force
 $env:Path += ";$($env:ProgramFiles)\aks-engine\"
